55663597e1961792e87895c15a0952984a45cb18
alexandrecbl <alexandrecb.leite@gmail.com>
2018-06-25 15:22:53 +0100

    Styling Nav component

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -3,16 +3,16 @@ import {NavLink} from 'react-router-dom';
 import LoadingDots from './LoadingDots';
 
 const Header = ({loading}) => {
-  return (
-    <nav>
-      <NavLink to="/" activeClassName="active" exact={true}>Home</NavLink>
-      {" | "}
-      <NavLink to="/movies" activeClassName="active">Movies</NavLink>
-      {" | "}
-      <NavLink to="/about" activeClassName="active">About</NavLink>
-      {loading && <LoadingDots interval={100} dots={20}/>}
-    </nav>
-  );
+    return (
+        <header>
+            <nav>
+                <NavLink to="/" activeClassName="active" exact={true}>Home</NavLink>
+                <NavLink to="/movies" activeClassName="active">Movies</NavLink>
+                <NavLink to="/about" activeClassName="active">About</NavLink>
+                {loading && <LoadingDots interval={100} dots={20}/>}
+            </nav>
+        </header>
+    );
 };
 
 export default Header;
